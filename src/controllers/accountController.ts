6ab805eb45e27241502a84b5bca09716eb474a70
tunjiNg01 <nahtlosng@gmail.com>
2023-07-17 12:47:17 +0100

    wallet implementation completed

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -9,27 +9,27 @@ export  const fundAccount = async (req: Request, res: Response, next: NextFuncti
     
      
      //2) check if the amount is defined
-     if (!amount) {
+     if (!amount || amount < 0 || typeof amount !== "number") {
          return next(new SystemError("Invalid request", 400))
      }
-     amount = parseFloat(amount)
      //3) perform transaction
     await knex.transaction(async transact => {
-       const balanceData = await transact.select("walletBalance", "_id").from("account").where("userId", user._id)
+        const balanceData = await transact.select("walletBalance", "_id").from("account").where("userId", user._id)
        const newBalance = balanceData[0].walletBalance + amount
        await transact("account").update("walletBalance", newBalance).where("_id", balanceData[0]._id)
        await transact("transactionHistory").insert({userId:user._id, balance: newBalance, amount: amount, type: "credit" })
-    })
+       
+      })
 
     return res.status(200).json({
         status: "success",
         message: "your transaction was successful",
       });
    } catch (error) {
-    console.log(error);
+    // console.log(error);
     return res.status(200).json({
         status: "success",
-        message: "your transaction was successful",
+        message: "your transaction was not successful",
       });
    }
    
@@ -42,34 +42,41 @@ export  const transferFunds = async (req: Request, res: Response, next: NextFunc
           //1) deconstruct request body
      let {designatedAccountId, amount, user} = req.body
      //2) check if the amount is defined
-     if (!amount) {
+     if (!amount || !designatedAccountId || amount < 0 || designatedAccountId <= 0 || typeof amount !== "number" || typeof designatedAccountId !== "number") {
         return next(new SystemError("Invalid request", 400))
     }
 
-    amount = parseFloat(amount)
       //3) perform transaction
       await knex.transaction(async transact => {
 
         const balanceData = await transact.select("walletBalance", "_id").from("account").where("userId", user._id)
+        
         if (balanceData[0].walletBalance >= amount) {
+           // deduct amount from user account and record it
             const newBalance = balanceData[0].walletBalance - amount
-            await transact("account").update("walletBalance", newBalance).where("_id", balanceData[0]._id)
+           await transact("account").update("walletBalance", newBalance).where("_id", balanceData[0]._id)
+           
             await transact("transactionHistory").insert({userId:user._id, balance: newBalance, amount: amount, type: "debit" })
-
+           // add amount to the designated account and record it
+            const accountToTransfer = await transact.select("walletBalance", "_id").from("account").where("userId", designatedAccountId)
+            const balanceAfterTransfer = accountToTransfer[0].walletBalance + amount
+            await transact("account").update("walletBalance", balanceAfterTransfer).where("userId", designatedAccountId)
+            await transact("transactionHistory").insert({userId: designatedAccountId, balance: balanceAfterTransfer, amount: amount, type: "credit" })
         }else {
-
+          transact.rollback(new SystemError("Insufficient balance", 400))
         }
-          const accountToTransfer = await transact.select("walletBalance", "_id").from("account").where("userId", designatedAccountId)
-          const balanceAfterTransfer = accountToTransfer[0].walletBalance + amount
-          await transact("account").update("walletBalance", balanceAfterTransfer).where("userId", designatedAccountId)
-          await transact("transactionHistory").insert({userId: designatedAccountId, balance: balanceAfterTransfer, amount: amount, type: "credit" })
+         
         })
+        return res.status(200).json({
+          status: "success",
+          message: "your transaction was successful",
+        });
  
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         return res.status(200).json({
             status: "success",
-            message: "your transaction was successful",
+            message: "your transaction was not successful",
           });
         
     }
@@ -77,5 +84,49 @@ export  const transferFunds = async (req: Request, res: Response, next: NextFunc
 }
 
 export  const withdrawFunds = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    //1) deconstruct request body
+    let { amount, user} = req.body;
+   
     
+    //2) check if the amount is defined
+    if (!amount || amount < 0 || typeof amount !== "number") {
+        return next(new SystemError("Invalid request", 400))
+    }
+    //3) perform transaction
+   await knex.transaction(async transact => {
+       const balanceData = await transact.select("walletBalance", "_id").from("account").where("userId", user._id)
+       //check if balance is greater than amount and perform transaction
+       if (balanceData[0].walletBalance >= amount) {
+        const newBalance = balanceData[0].walletBalance - amount
+        await transact("account").update("walletBalance", newBalance).where("_id", balanceData[0]._id)
+        await transact("transactionHistory").insert({userId:user._id, balance: newBalance, amount: amount, type: "debit" })
+       
+        
+       }else{
+        transact.rollback(new SystemError("invalid amount provided", 400))
+       }
+     })
+     return res.status(200).json({
+      status: "success",
+      message: "your withdrawal was successful",
+    });
+
+   
+  } catch (error) {
+  //  console.log(error);
+   return res.status(200).json({
+       status: "success",
+       message: "your transaction was not successful due to insufficient balance",
+     });
+  }
+}
+
+export const paymentGateway = async (req: Request, res: Response, next: NextFunction) => {
+  // this is where payment gateway integration can comes in
+  console.log("process payment...");
+  console.log("payment processing is successful");
+
+  next()
+  
 }
--- src/routes/accountRoute.ts
+++ src/routes/accountRoute.ts
@@ -1,12 +1,12 @@
 import { routes } from "../helper/routes";
-import {fundAccount, withdrawFunds, transferFunds} from "../controllers/accountController";
+import {fundAccount, withdrawFunds, transferFunds, paymentGateway} from "../controllers/accountController";
 import { tokenValidator } from "../controllers/authController";
 
 // this middleware is used to protect the routes 
 // and its also introduce the user auth user data to the request object body 
 routes.use(tokenValidator)
-routes.post("/fund-account", fundAccount)
-routes.post("/withdraw-funds", withdrawFunds)
-routes.post("/transfer-funds", transferFunds)
+routes.post("/fund-account",paymentGateway, fundAccount);
+routes.post("/withdraw-funds",paymentGateway, withdrawFunds);
+routes.post("/transfer-funds",paymentGateway, transferFunds);
 
 export default routes
