a47807a0853b1137e9af4a6e67465d8586637950
tunjiNg01 <nahtlosng@gmail.com>
2023-07-18 15:25:17 +0100

    update on port

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV == "production") {
     
 }
 
-const port = process.env.PORT 
+const port = process.env.PORT || 1337
 
 export default app.listen(port, () => {
     console.log(`now listening on port! ${port}`);
