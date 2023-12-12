898fa5e7fbff9e25c8c06ea6b7d76c544b0c2cf9
tunjiNg01 <nahtlosng@gmail.com>
2023-12-12 08:10:10 +0100

    bug fixes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-import dotenv from "dotenv"
-import app from "./app"
-dotenv.config()
-
+import dotenv from "dotenv";
+import app from "./app";
+dotenv.config();
 
 if (process.env.NODE_ENV == "production") {
-    console.log("production");
-    
+  console.log("production");
 }
 
-const port = process.env.PORT || 1337
+const port = process.env.PORT || 1337;
 
 export default app.listen(port, () => {
-    console.log(`now listening on port! ${port}`);
-})
+  console.log(`now listening on port! ${port}`);
+});
